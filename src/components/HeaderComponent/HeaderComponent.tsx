5734cf0cf43b4c6af52b9beeb194d6d928bed76c
BrBrov <epoc@list.ru>
2025-04-02 20:52:53 +0300

    feat(HeaderComponent): create Header component

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,12 +1,17 @@
 import { chakra, ChakraComponent } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 
+import LogoComponent from '../LogoComponent/LogoComponent';
 import headerStyles from './HeaderComponentStyles';
 
 const HeaderChakra: ChakraComponent<'header', object> = chakra('header');
 
 function HeaderComponent(): ReactElement {
-    return <HeaderChakra sx={headerStyles}></HeaderChakra>;
+    return (
+        <HeaderChakra sx={headerStyles}>
+            <LogoComponent />
+        </HeaderChakra>
+    );
 }
 
 export default HeaderComponent;
--- src/components/HeaderComponent/HeaderComponentStyles.ts
+++ src/components/HeaderComponent/HeaderComponentStyles.ts
@@ -4,6 +4,11 @@ const headerStyles: SystemStyleObject = {
     display: 'flex',
     w: '100%',
     h: { base: '80px', md: '64px', sm: '64px' },
+    boxSizing: 'border-box',
+    paddingLeft: { base: '16px', md: '20px' },
+    paddingRight: { base: '56px', md: '20px', sm: '16px' },
+    justifyContent: 'space-between',
+    alignItems: 'center',
     backgroundColor: '#ffffd3',
 };
 
