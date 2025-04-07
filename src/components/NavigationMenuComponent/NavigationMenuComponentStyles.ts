6fb6d4d69888455a4699eb06ceb31a6b4c4f35fe
BrBrov <epoc@list.ru>
2025-04-07 22:30:30 +0300

    fix(NavigationMenuComponent): fix scrollbar mark up

--- src/components/NavigationMenuComponent/NavigationMenuComponentStyles.ts
+++ src/components/NavigationMenuComponent/NavigationMenuComponentStyles.ts
@@ -8,6 +8,21 @@ const navigationMenuComponentStyles: SystemStyleObject = {
     top: { base: '64px', md: '104px' },
     flexDirection: 'column',
     overflowY: 'auto',
+    '&::-webkit-scrollbar-button': {
+        w: '0',
+        h: '0',
+    },
+    '::-webkit-scrollbar': {
+        appearance: 'none',
+        w: '8px',
+        borderRadius: '8px',
+        backgroundColor: 'rgba(0, 0, 0, 0.04)',
+    },
+    '::-webkit-scrollbar-thumb': {
+        w: '8px',
+        borderRadius: '8px',
+        backgroundColor: 'rgba(0, 0, 0, 0.16)',
+    },
 };
 
 export default navigationMenuComponentStyles;
