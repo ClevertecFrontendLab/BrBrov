2ec4b9323e83c2426dc4ce5449fd808c38b9ed7f
BrBrov <epoc@list.ru>
2025-04-10 20:25:05 +0300

    fix(Icons*): fix svg mark up's

--- src/custom-icons/BurgerIcon.tsx
+++ src/custom-icons/BurgerIcon.tsx
@@ -1,18 +1,8 @@
-import { createIcon } from '@chakra-ui/react';
+import { createIcon, IconProps } from '@chakra-ui/react';
 
-const BurgerIcon: React.FC = createIcon({
+const BurgerIcon: React.FC<IconProps> = createIcon({
     displayName: 'BurgerIcon',
-    path: (
-        <svg
-            width='16'
-            height='12'
-            viewBox='0 0 16 12'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-        >
-            <path d='M0 0H16V2H0V0ZM0 5H16V7H0V5ZM0 10H16V12H0V10Z' fill='black' />
-        </svg>
-    ),
+    path: <path d='M0 0H16V2H0V0ZM0 5H16V7H0V5ZM0 10H16V12H0V10Z' fill='black' />,
     defaultProps: {
         viewBox: '0 0 16 12',
         width: '16px',
